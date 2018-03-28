d35410d068941f50f68173a7fe33421af716b742
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-03-28 23:39:33 +0400

    Citrus Soap Simulator

---
+++ src/main/java/io/github/devbhuwan/EndpointConfig.java
@@ -0,0 +1,135 @@
+package io.github.devbhuwan;
+
+import com.consol.citrus.config.CitrusSpringConfig;
+import com.consol.citrus.dsl.endpoint.CitrusEndpoints;
+import com.consol.citrus.endpoint.EndpointAdapter;
+import com.consol.citrus.endpoint.adapter.RequestDispatchingEndpointAdapter;
+import com.consol.citrus.endpoint.adapter.StaticResponseEndpointAdapter;
+import com.consol.citrus.endpoint.adapter.mapping.HeaderMappingKeyExtractor;
+import com.consol.citrus.endpoint.adapter.mapping.SimpleMappingStrategy;
+import com.consol.citrus.endpoint.adapter.mapping.SoapActionMappingKeyExtractor;
+import com.consol.citrus.variable.GlobalVariables;
+import com.consol.citrus.ws.client.WebServiceClient;
+import com.consol.citrus.ws.interceptor.LoggingClientInterceptor;
+import com.consol.citrus.ws.server.WebServiceServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.ws.server.EndpointInterceptor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@ComponentScan(basePackageClasses = EndpointConfig.class)
+@Import(CitrusSpringConfig.class)
+public class EndpointConfig {
+
+    private static final int PORT = 8080;
+    private static final Logger LOG = LoggerFactory.getLogger(EndpointConfig.class);
+    private static final String WS_CBS_URI = "/services/ws/CBS/v1";
+
+    @Bean
+    public GlobalVariables globalVariables() {
+        GlobalVariables variables = new GlobalVariables();
+        variables.getVariables().put("todoId", "702c4a4e-5c8a-4ce2-a451-4ed435d3604a");
+        variables.getVariables().put("todoName", "todo_1871");
+        variables.getVariables().put("todoDescription", "Description: todo_1871");
+        return variables;
+    }
+
+    @Bean
+    public LoggingClientInterceptor clientLoggingInterceptor() {
+        return new LoggingClientInterceptor();
+    }
+
+
+    @Bean
+    public WebServiceClient cbsClient() {
+        String uri = "http://localhost:" + PORT + WS_CBS_URI;
+        LOG.info("------------------CBS CLIENT---------------------");
+        LOG.info("cbsClient uri=[{}]", uri);
+        LOG.info("------------------CBS CLIENT---------------------");
+        return CitrusEndpoints.soap()
+                .client()
+                .defaultUri(uri)
+                .interceptor(clientLoggingInterceptor())
+                .build();
+    }
+
+    @Bean
+    public WebServiceServer cbsServer(List<EndpointInterceptor> interceptors) throws Exception {
+        LOG.info("------------------CBS SERVER---------------------");
+        LOG.info("Running CBS Server at port [{}]", PORT);
+        LOG.info("------------------CBS SERVER---------------------");
+        return CitrusEndpoints.soap()
+                .server()
+                .port(PORT)
+                .endpointAdapter(dispatchingEndpointAdapter())
+                .interceptors(interceptors)
+                .timeout(10000)
+                .autoStart(true)
+                .build();
+    }
+
+    @Bean
+    public RequestDispatchingEndpointAdapter dispatchingEndpointAdapter() {
+        RequestDispatchingEndpointAdapter dispatchingEndpointAdapter = new RequestDispatchingEndpointAdapter();
+        dispatchingEndpointAdapter.setMappingKeyExtractor(mappingKeyExtractor());
+        dispatchingEndpointAdapter.setMappingStrategy(mappingStrategy());
+        return dispatchingEndpointAdapter;
+    }
+
+    @Bean
+    public HeaderMappingKeyExtractor mappingKeyExtractor() {
+        return new SoapActionMappingKeyExtractor();
+    }
+
+    @Bean
+    public SimpleMappingStrategy mappingStrategy() {
+        SimpleMappingStrategy mappingStrategy = new SimpleMappingStrategy();
+
+        Map<String, EndpointAdapter> mappings = new HashMap<>();
+
+        mappings.put("urn:getTodo", todoResponseAdapter());
+        mappings.put("urn:getTodoList", todoListResponseAdapter());
+
+        mappingStrategy.setAdapterMappings(mappings);
+        return mappingStrategy;
+    }
+
+    @Bean
+    public EndpointAdapter todoResponseAdapter() {
+        StaticResponseEndpointAdapter endpointAdapter = new StaticResponseEndpointAdapter();
+        endpointAdapter.setMessagePayload("<getTodoResponse xmlns=\"http://citrusframework.org/samples/todolist\">" +
+                "<todoEntry xmlns=\"http://citrusframework.org/samples/todolist\">" +
+                "<id>${todoId}</id>" +
+                "<title>${todoName}</title>" +
+                "<description>${todoDescription}</description>" +
+                "<done>false</done>" +
+                "</todoEntry>" +
+                "</getTodoResponse>");
+        return endpointAdapter;
+    }
+
+    @Bean
+    public EndpointAdapter todoListResponseAdapter() {
+        StaticResponseEndpointAdapter endpointAdapter = new StaticResponseEndpointAdapter();
+        endpointAdapter.setMessagePayload("<getTodoListResponse xmlns=\"http://citrusframework.org/samples/todolist\">" +
+                "<list>" +
+                "<todoEntry>" +
+                "<id>${todoId}</id>" +
+                "<title>${todoName}</title>" +
+                "<description>${todoDescription}</description>" +
+                "<done>false</done>" +
+                "</todoEntry>" +
+                "</list>" +
+                "</getTodoListResponse>");
+        return endpointAdapter;
+    }
+
+}
---
+++ src/main/java/io/github/devbhuwan/Simulator.java
@@ -0,0 +1,18 @@
+package io.github.devbhuwan;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simulator implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Simulator.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
